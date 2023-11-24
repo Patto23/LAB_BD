b4cbb0457005d5236d95b163c0fe43fea88d19fd
Tania Patricia Carabantes Garcia <128654272+Patto23@users.noreply.github.com>
2023-11-23 23:02:01 -0600

    Guia_9
    Vistas

---
+++ Guia_9/Vistas.sql
@@ -0,0 +1,9 @@
+/*Vistas: Mostrar empleados con su respectiva dirección.*/
+
+create view Empleados_view 
+as select id_empleado, CONCAT(A.Nombres, ' ', A.Apellidos) as nombre_empleado,
+B.Linea1 as Direccion1, B.Linea2 as Direccion2, B.CP
+from dbo.Empleados A, dbo.Direcciones B where A.ID_Direccion = B.ID_Direccion;
+
+
+--select* from dbo.Empleados_view;
